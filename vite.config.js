1c6d046b4c6803e142ffdf9102051f496fa6f49a
ssongjiyeon <b8480@naver.com>
2024-10-28 19:29:25 +0900

    #3 - 운영 / 개발 포트 변경

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.VITE_PORT) || 81,
+  },
+  preview: {
+    port: parseInt(process.env.VITE_PORT) || 80,  // Vite의 preview 모드에서 프로덕션 포트 사용
+  }
 })
